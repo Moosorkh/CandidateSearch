6c3fec8e27b9bcbbd07ed6d560903bca5a38ea9d
Fortis Irdmousa <moosorkh@gmail.com>
2024-11-09 13:21:59 -0800

    Nav styling added

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+  //  Add necessary code to display the navigation bar and link between the pages
+  const location = useLocation();
   return (
-    <nav className="nav-bar">
-      <Link to="/" className="nav-link">
-        Candidate Search
-      </Link>
-      <Link to="/SavedCandidates" className="nav-link">
-        Saved Candidates
-      </Link>
+    <nav className="nav">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link
+            to="/"
+            className={`nav-link ${location.pathname === "/" ? "active" : ""}`}
+          >
+            Candidate Search
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/SavedCandidates"
+            className={`nav-link ${
+              location.pathname === "/SavedCandidates" ? "active" : ""
+            }`}
+          >
+            Saved Candidates
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
